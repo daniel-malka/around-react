92027ca92e2ff1e0138ce17928da4e7203d06273
Daniel <daniel847@tutanota.com>
2022-11-03 22:34:32 +0200

    fixfiles

--- src/components/PopupWithImage.jsx
+++ src/components/PopupWithImage.jsx
@@ -1,26 +1,28 @@
 import close from "../images/close-X.svg";
+
 import "../blocks/popup.css";
+
 function PopupWithImage(props) {
   return (
-    <div className={`popup popup__img ${props.isOpen && "popup_open"}`}>
+    <div className={`popup popup_type_image  ${props.isOpen && "popup_open"}`}>
       <div className="popup__container-img popup__container_type_image">
-        <button
-          className="popup__close popup__close-button_type_image"
-          type="button"
-        >
+        <span className=" popup__close-button_type_image" type="button">
           <img
             src={close}
             alt=""
             className="popup__close-img"
             onClick={props.onClose}
           />
-        </button>
+        </span>
+
         <figure>
           <img src={props.card.link} alt="#" className="popup__img" />
-          <figcaption className="popup__caption">{props.name}</figcaption>
+
+          <figcaption className="popup__title">{props.card.name}</figcaption>
         </figure>
       </div>
     </div>
   );
 }
+
 export default PopupWithImage;
